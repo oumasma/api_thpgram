5f6b1103295916b2bdcc20f8e37fd93c61dea47e
oumasma <oumasma93@hotmail.fr>
2020-11-25 15:36:27 +0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  default_url_options :host => "http://localhost:3000/"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/login',
+      sign_out: 'api/logout',
+      registration: 'api/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
+end
